f25760799c716e1d0399f1ccf49fd9ea28b1b57b
Ashesh Vidyut <ashesh.vidyut@hashicorp.com>
2024-07-07 19:34:36 +0530

    fix solution

--- solution.cpp
+++ solution.cpp
@@ -31,43 +31,9 @@ const int MOD = (int)1e9 + 7;
 
 class Solution {
 public:
-    double calc(double x, long a, long b, long c, vector<vector<int>> &xy) {
-        double y = -1 * ( c + a * x) / b;
-        double sum = 0.0;
-        for(auto p : xy) {
-            double num = pow(p[0]-x,2) + pow(p[1]-y,2);
-            sum = sum + sqrt(num);
-        }
-        return sum;
-    }
     void solve() {
         int tc; cin >> tc;
         while(tc--) {
-            int n; double a, b, c; cin >> n;
-            cin >> a >> b >> c;
-            vector<vector<int>> xy;
-            for (int i = 0; i < n; ++i) {
-                vector<int> ar(2);
-                cin >> ar[0] >> ar[1];
-                xy.push_back(ar);
-            }
-            double ans = -1e9;
-            double l = -5e3, r = 5e3;
-            while((r - l) >= (1e-6)) {
-                double x1 = l + ((r - l) / 3);
-                double x2 = r - ((r - l) / 3);
-                double c1 = calc(x1, a, b, c, xy);
-                double c2 = calc(x2, a, b, c, xy);
-                if (c1 < c2) {
-                    ans = x1;
-                    r = x2;
-                }
-                else {
-                    ans = x2;
-                    l = x1;
-                }
-            }
-            cout << fixed << setprecision(6) << calc(ans, a, b, c, xy) << endl;
         }
     }
 };
